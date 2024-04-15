e27ca2b5fd16881cc0459d1db6b8db1aaa58d769
Van Nhan Nguyen <nhan@centrum.cz>
2024-04-15 21:51:46 +0200

    Game Manager frog tracking
    - FrogSpawner class now notifies the Game Manager of frog summoning.
    - Game Manager keeps track of which frogs have been summoned and which not.

--- Assets/System/GameManager.cs
+++ Assets/System/GameManager.cs
@@ -1,22 +1,51 @@
 using System.Collections;
 using System.Collections.Generic;
+using System.Linq;
 using UnityEngine;
 
 public class GameManager : MonoBehaviour
 {
+    public enum FrogKnight
+    {
+        Croakaint = 0,
+        Fromapond = 1,
+        Hopsalot = 2,
+        Ribbitan = 3
+    }
+
     public static GameManager Instance;
 
     public GameObject player;
 
-    public Vector3 SavedCoords;
+    private Dictionary<FrogKnight, bool> m_isSummoned = new Dictionary<FrogKnight, bool>();
 
-    // Memory Game
-    public bool IsSirCroakaintSpawned = false;
-
-    private void Awake() { if (Instance == null) Instance = this; }
-
-    public void SaveCoords(Vector3 coords)
+    private void Awake()
     {
-        SavedCoords = coords;
+        if (Instance == null) Instance = this;
+        foreach (FrogKnight id in FrogKnight.GetValues(typeof(FrogKnight))) m_isSummoned[id] = false;
     }
+
+    public void NotifyOfSummoning(GameObject obj)
+    {
+        if (obj.GetComponent<FollowerFrog>())
+        {
+            switch (obj.name)
+            {
+                case "Sir Croakaint":
+                    m_isSummoned[FrogKnight.Croakaint] = true;
+                    break;
+                case "Sir Fromapond":
+                    m_isSummoned[FrogKnight.Fromapond] = true;
+                    break;
+                case "Sir Hopsalot":
+                    m_isSummoned[FrogKnight.Hopsalot] = true;
+                    break;
+                case "Sir Ribbitan":
+                    m_isSummoned[FrogKnight.Ribbitan] = true;
+                    break;
+            }
+        }
+    }
+
+    public bool IsFrogSpawned(FrogKnight id) { return m_isSummoned[id]; }
 }
