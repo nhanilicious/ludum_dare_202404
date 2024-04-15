cc519fd5d6c5ec526226dd38c1e60f2c1e4f95f2
Van Nhan Nguyen <nhan@centrum.cz>
2024-04-15 16:49:09 +0200

    Jumping Puzzle (Sir Fromapond)
    - Jumping Puzzle inserted into the Swamp Level.
    - Updated the FrogSpawner to use Transform variable for spawn location instead of Vector3.
    - Added Jumping Puzzle prefabs and variants, which include both the Magic Orb and frog spawn location object.
    - Removed redundant Magic Orb prefabs.

--- Assets/Models/Minigames/FrogSpawner.cs
+++ Assets/Models/Minigames/FrogSpawner.cs
@@ -7,13 +7,13 @@ public abstract class FrogSpawner : MonoBehaviour
     public GameObject frogToBeSpawned;
     public GameObject frogSpawnEffect;
 
-    public Vector3 frogSpawnPosition;
+    public Transform frogSpawnPosition;
 
     private const float EFFECT_DURATION = 1.406f;
 
     protected void SpawnFrog()
     {
-        Instantiate(frogToBeSpawned, frogSpawnPosition, transform.rotation);
-        Destroy(Instantiate(frogSpawnEffect, frogSpawnPosition, transform.rotation), EFFECT_DURATION);
+        Instantiate(frogToBeSpawned, frogSpawnPosition.position, transform.rotation);
+        Destroy(Instantiate(frogSpawnEffect, frogSpawnPosition.position, transform.rotation), EFFECT_DURATION);
     }
 }
